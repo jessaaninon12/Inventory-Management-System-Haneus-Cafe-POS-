da43df58112f34a45c088f7f58ed7d88cefddff1
JM Suic0 <suicojm99@gmail.com>
2026-03-04 15:25:33 +0800

    backend for login page

--- BACKEND/pos_core/wsgi.py
+++
@@ -1,9 +0,0 @@
-"""WSGI config for Haneus Cafe POS."""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pos_core.settings")
-
-application = get_wsgi_application()
